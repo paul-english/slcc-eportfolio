2766078ef008e969eb74ffbdb375ccc7bafd2d1a
Paul English <paul@onfrst.com>
2013-02-10 11:04:43 -0700

    remove skeleton file

--- sp13-cs-2420-003-baird/project-02-basic-list-operations-arrays/skeleton.java
+++
@@ -1,9 +0,0 @@
-// Comments, indentation and placement of {} per Style Guide.
-// Single-subscripted generic array used as backing store.
-// Constructors: default capacity and specific capacity.
-// Traversal operation addressed.
-// Resize operation - automatic and forced.
-// Replace operation - by position and specific value.
-// Insert operation - at end and at specific position.
-// Delete operation - by position and specific value.
-// Test application using arrays of at least two different types.

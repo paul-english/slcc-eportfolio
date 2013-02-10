e924803328ace6dad97ab6988c7a29c5980b974c
Paul English <paul@onfrst.com>
2013-02-10 10:56:38 -0700

    SingleLinkedList project skeleton

---
+++ sp13-cs-2420-003-baird/project-03-generic-single-linked-list/GenericSingleLinkedList/src/genericsinglelinkedlist/GenericSingleLinkedList.java
@@ -0,0 +1,40 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package genericsinglelinkedlist;
+
+/**
+ *
+ * @author nrub
+ */
+public class GenericSingleLinkedList
+{
+	/**
+	 * Problem.
+	 * Write a program that maintains a list of items using a generic
+	 * single-linked list as the backing store.
+	 *
+	 * Specification.
+	 * 1. Use the SinglyLinkedList<E> class provided in the text.
+	 * 2. The type of item stored in the list can be any item of your choice:
+	 *    pets, books, coins, friends, etc. (You'll need to write the class
+	 *    representing the item).
+	 * 3. Run the program from a menu with the following options:
+	 *    * Add an item.
+	 *    * Delete an item.
+	 *    * Find an item.
+	 *    * Display the list.
+	 *    * Exit
+	 * 4. When the program runs, programatically create 5 items and add them
+	 *    to the list before the menu is displayed.
+	 */
+
+	/**
+	 * @param args the command line arguments
+	 */
+	public static void main(String[] args)
+	{
+		// TODO code application logic here
+	}
+}
--- sp13-cs-2420-003-baird/project-03-generic-single-linked-list/skeleton.java
+++
@@ -1,9 +0,0 @@
-// Comments, indentation and placement of {} per Style Guide.
-// Uses Gray's SinglyLinkedList class
-// Custom class for items stored in the list.
-// Menu created as specified.
-// Add operation implemented correctly.
-// Delete operation implemented correctly.
-// Find operation iimplemented correctly.
-// Display operation implemented correctly.
-// Five items programatically added to list before menu runs.

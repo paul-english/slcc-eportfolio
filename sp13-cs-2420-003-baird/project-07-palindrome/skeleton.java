9cfb9540aa518f19e11ce08c2098907531a710d3
Paul English <paul@onfrst.com>
2013-03-06 18:38:47 -0700

    Remove skeleton files

--- sp13-cs-2420-003-baird/project-07-palindrome/skeleton.java
+++
@@ -1,9 +0,0 @@
-// Comments, indentation and placement of {} per Style Guide.
-// Create MyStack implementing Stack.
-// Create MyQueue implementing Queue.
-// Create and implement a test plan for MyStack.
-// Create and implement a test plan for MyQueue.
-// Get an input string from the user.
-// Ignore white space and differences in case.
-// Determine if the input string is a palindrome.
-// Submit an executable JAR file that includes all .java files.
--- sp13-cs-2420-003-baird/project-10-binary-search-tree/skeleton.java
+++
@@ -1,9 +0,0 @@
-// Comments, indentation, placement of {} per Style Guide.
-// Student class defined as specified.
-// Menu created as specified.
-// Student records stored in a binary search tree (BST) ordered by last name, first name.
-// Student records automatically read into BST each time program runs.
-// Student records automatically written when program ends.
-// Custom Visitor object to write a Student record.
-// Javadoc comments/documentation created.
-// Executable .jar file submitted.
